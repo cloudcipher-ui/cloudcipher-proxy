c36057f0c852d83dff3162814463541959609bf9
ana117 <63540951+ana117@users.noreply.github.com>
2024-03-20 16:23:16 +0700

    Initialize project

---
+++ src/main/java/com/cloudcipher/cloudcipher_proxy/CloudcipherProxyApplication.java
@@ -0,0 +1,13 @@
+package com.cloudcipher.cloudcipher_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudcipherProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudcipherProxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloudcipher/cloudcipher_proxy/CloudcipherProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudcipher.cloudcipher_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudcipherProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
